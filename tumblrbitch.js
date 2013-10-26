4f487d19f5e4d457b50a1fea259d425d07bbee4a
Pedro Sola <p3dro.sola@gmail.com>
2013-10-26 14:34:12 +0200

    Port 3000

--- tumblrbitch.js
+++ tumblrbitch.js
@@ -1,6 +1,6 @@
-var port = process.env.PORT || 5000,
+var port = process.env.PORT || 3000,
 server = require('./src/server').createServer();
 server.listen(port);
 
-console.log('\n>> Startup completed!')
+console.log('\n>> Startup completed!');
 console.log('Listening on port', port);
